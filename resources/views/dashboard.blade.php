3a852ff1f095bee31c40d7cf05b8f73ee4461082
Arnold Boston <aboston@koodi.ph>
2018-01-03 12:21:14 +0800

    fix minor

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,15 +10,17 @@
             </div>
         </div>
     </div>
-    <div class="col-md-4">
-        <div class="well">
-            <i class="fa fa-shopping-cart fa-4x fa-fw" aria-hidden="true"></i>
-            <div class="text">
-                <h3>Stocks</h3>
-                <p>Items : 200</p>
+    <a href="{{ route('stock.index')}}">
+        <div class="col-md-4">
+            <div class="well">
+                <i class="fa fa-shopping-cart fa-4x fa-fw" aria-hidden="true"></i>
+                <div class="text">
+                    <h3>Stocks</h3>
+                    <p>Items : 200</p>
+                </div>
             </div>
         </div>
-    </div>
+    </a>
     <div class="col-md-4">
         <div class="well">
             <i class="fa fa-exchange fa-4x fa-fw" aria-hidden="true"></i>
