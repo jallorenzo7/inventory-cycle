68ed6c6f994a82f9aed7ad599dc9a6e441b98af1
Arnold Boston <aboston@koodi.ph>
2018-02-22 22:15:34 +0800

    x

--- resources/views/mission_vision.blade.php
+++ resources/views/mission_vision.blade.php
@@ -1,15 +1,17 @@
 @extends('layouts.app')
 @section('content')
 <div class="row">
-    <div class="col-md-12">
+    <div class="col-md-3"></div>
+    <div class="col-md-6">
         <center>
         <h1>Mission</h1>
-        <p>Description</p>
+        <p class="well">Is to be number one Dealer in pampanga.</p>
         <hr>
         <hr>
         <h1>Vision</h1>
-        <p>Description</p>
+        <p class="well">To be the go-to company for motors, starters & drive As with all excellent organizations, GM Cycle Motors & Control earns success by working as a cohesive team, focused on a common mission and operating with shared values.</p>
         </center>
     </div>
+    <div class="col-md-3"></div>
 </div>
 @endsection
